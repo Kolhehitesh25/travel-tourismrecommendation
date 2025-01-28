41ffea480e8f3027ea8fe656e2e479ecb791db27
hitesh kolhe <hiteshkohle25gmail.com>
2025-01-28 23:48:38 +0530

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Assuming you'll use React Router for navigation
+import { Link, useNavigate } from 'react-router-dom'; // Assuming you'll use React Router for navigation
 import './Navbar.css';
+
 const Navbar = () => {
+
+    
     return (
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/register">Register</Link></li>
+                
                 <li><Link to="/signin">Sign In</Link></li>
                 {/* Add more navigation links as needed */}
             </ul>
